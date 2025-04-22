f6e68999f6906d8c135943ddf91fc9254cea0acc
Dacho688 <95297394+Dacho688@users.noreply.github.com>
2025-04-21 21:21:47 -0500

    Update model to llama 3.3 from 3.1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ---
-title: Resume Chatbot
+title: Resume Chatbot 
 emoji: 🗎
 colorFrom: blue
 colorTo: yellow
@@ -7,8 +7,11 @@ sdk: gradio
 sdk_version: 4.22.0
 app_file: app.py
 pinned: false
-license: apache-2.0
-short_description: Davor's resume chatbot
+short_description: Davor's resume chatbot powered by Llama-3.3-70B-Instruct
 ---
 
-App link: https://huggingface.co/spaces/dkondic/ResumeChatbot
+# Resume Chatbot
+
+This project aims to illustrate AI's contextual awareness with private and/or domain specific AI knowledgebases (ie data not used durring model training). We utilize Meta's Llama-3.3-70B-Instruct open source large language model and feed it with external data, in this case my resume. We then build a Python based Gradio web app and deploy it to Huggingface Spaces. A github workflow is used to push any main branch commits to the Huggingface space automatically.
+
+<a href="https://dkondic-resumechatbot.hf.space/">Try APP</a>
--- app.py
+++ app.py
@@ -1,60 +1,92 @@
 import os
 import gradio as gr
 import spaces
-from huggingface_hub import InferenceClient,login
+from huggingface_hub import InferenceClient,login,whoami
 
-login(os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+if not whoami():
+    login(os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+    
 client=InferenceClient()
 
 @spaces.GPU
 def chat(message: str,history: list[tuple[str, str]]):
-    #print(history)
-    print(message)
-    content = """You are Davor Kondic's resume chatbot. You will be interviewed by people for various positions.
+    print("User Message: ",message,"\n")
+    content = """You are Davor Kondic, a data analyst applying for jobs.
+                You will be interviewed by recruiters and hiring managers for various positions.
+                Use the provided resume as knowledge base to answer their questions and discuss your qualifications.
                 You only know what is in the resume! NOTHING ELSE!
                 Stay on topic and only answer questions related to what is in the resume.
-                Make me (Davor) look good!
-                Here is the resume:"""
-    resume = """Results-driven Data Science professional with over a decade of experience in extracting actionable insights from data. Proven track record of leveraging data analytics to inform business decisions and drive growth. Passionate about data-driven decision making and staying at the forefront of data science trends.
-
+                Here is the resume:
+                """
+    
+    resume = """Results-driven data professional with over a decade of experience in extracting actionable insights from data. Proven track
+record of leveraging data analytics to inform business decisions, minimize risks, costs, and drive growth. Passionate about
+data-driven decision making and staying at the forefront of data science trends.
 EDUCATION
-o	Master of Science in Data Science (In Progress), Northwestern University, Evanston, IL
-o	Bachelor of Science in Economics (Cum Laude), Northern Illinois University, DeKalb, IL
-
+o Master of Science in Data Science (In Progress), Northwestern University, Evanston, IL
+o Bachelor of Science in Economics (Cum Laude), Northern Illinois University, DeKalb, IL
 SKILLS
-o	Technical: Python, R, SQL, Tableau, Databricks, Hadoop, Excel, GitHub, AI 
-o	Professional: Data Science, Machine Learning, AI Development, Operational Research, Descriptive/Predictive/ Prescriptive Analytics, Data Engineering, Data Warehousing, Data Visualization, Agile Project Management (SCRUM)
-
+o Technical: Python, R, SQL, Markdown, HTML, YAML, Git, GitHub CI/CD, Docker, CLI, Batch script, Tableau, Hadoop
+HUE, LLM Transformers, PyTorch, Excel
+o Professional: Data Science, Machine Learning, AI App Development, Operational Research, Descriptive/Predictive/
+Prescriptive Analytics, Data Engineering, Data Warehousing, Data Visualization, SCRUM, CRISP-DM
+PORTFOLIO/SOCIAL
+o Project Portfolio Website: https://dacho688.github.io/
+o AI Demo Apps: https://huggingface.co/dkondic
+o GitHub Profile: https://github.com/Dacho688
+o LinkedIn: https://www.linkedin.com/in/davor-kondic-54576886/
 PROFESSIONAL EXPERIENCE
-AI Developer, Open-Source Foundation Models (2024 - Present)
-o	Designed and developed LLM AI agents using the Reasoning and Acting framework (ReAct), enabling autonomous decision-making and AI reasoning to solve real-world problems.
-o	Utilized open-source foundation transformer models, such as Llama 3.1 and Llava Next, to create custom AI solutions for various domains, including: Data Analysis, Image Generation, Image and Document chatbot
-o	Developed Retrieval Augmented Generation (RAG) AI agents by integrating AI with external tools, such as databases and APIs, to enable private and seamless domain specific AI knowledgebases
-o	AI Web Application Demos: https://huggingface.co/dkondic
-o	AI Web Application Code: https://github.com/Dacho688
-
-Supply Chain Specialist (2022), ALDI Inc., Batavia, IL
+Freelance AI Consultant/Developer, Open-Source Foundation Models (2024 - Present)
+o Utilized open-source LLM foundation models, such as Llama and Llava models, to create custom AI solutions for
+various domains, including Data Analysis, Data Visualization, Image and Document chatbots.
+o Designed and developed LLM AI agents and multi-agents using Reasoning and Acting framework (ReAct) and Chain of
+Thought (CoT), enabling autonomous AI decision-making and reasoning to solve real-world problems.
+o Developed Retrieval Augmented Generation (RAG) AI agents by integrating AI with external data, such as databases
+and APIs, to enable private and domain specific AI knowledgebases.
+o Fine-tuned open-source LLM models for specific domains and use cases.
+o Consulted clients regarding AI deployment, infrastructure (cloud vs on-premise), and open-source foundation models
+vs proprietary.
+o AI Demos: https://huggingface.co/dkondic
+o AI Code: https://github.com/Dacho688
 Supply Chain Analyst (2023 – 2024), ALDI Inc., Batavia, IL
-o	Utilized Python's powerful packages and APIs to extract, transform, and load data from various sources, enabling data-driven supply chain optimization.
-o	Cleaned, prepared, and analyzed logistic and business data to support management's strategic initiatives, demonstrating expertise in data wrangling and analysis.
-o	Successfully planned demand and inventory for ALDI's 3PW network, utilizing SARIMAX models to forecast sales and inventory levels.
-o	Developed and maintained end-to-end supply chain network optimization and cost analysis models, presenting findings to management and driving business decisions.
-o	Designed and implemented a SQL data warehouse and database for ALDI's 3PW network
-o	Created visually engaging Tableau data visualizations and reports, maintaining a Tableau server team subfolder for seamless distribution.
-o	Developed and maintained a custom ALDI Python package utilizing Gitlab's version control and package distribution capabilities.
-
+o Extracted, transformed, and loaded (ETL) data from various sources, mainly from SQL databases to Tableau cloud
+server utilizing Python's powerful packages and APIs (pyodbc, smtplib, tableauhyperapi, tableauserverclient, pandas,
+numpy ect)
+o Designed and implemented a SQL data warehouse and database for ALDI's Thirds Party Warehouse (3PW) network.
+o Developed, maintained, and deployed a custom ALDI Python library utilizing Git version control and distribution
+capabilities.
+o Planned and reported via ETL automation demand and inventory for all 26 of ALDI's Thirds Party Warehouses (3PW).
+o Forecasted sales and inventory levels enabling flexible and real time decision making.
+o Developed and maintained end-to-end supply chain network optimization and cost analysis models, presenting
+findings to management and driving business decisions.
+o Continued to perform and improve my duties as a Specialist with Pythonic automation
+Supply Chain Specialist (2022), ALDI Inc., Batavia, IL
+o Extracted, transformed, and loaded (ETL) logistic and business data to support management's decisions for strategic
+initiatives, demonstrating expertise in data wrangling, analysis, and business understanding.
+o Created and maintained Tableau data visualizations and Excel reports that were automatically updated on an agreed
+upon cadence.
+o Queried, cleaned, and analyzed ad hoc data and reports as needed.
+o Performed supply chain cost analysis for ALDI’s existing 3PW logistics network to make it more efficient and optimal
+o Automated ETL pipelines and reports with Python’s schedule library.
 Senior Accounting Data Analyst (Contract), Everywhere Wireless, Chicago, IL (2020)
-o	Extracted, transformed, and analyzed accounting, inventory, sales, and customer data from multiple sources (Quick Books Online, Fishbowl, V-Tiger)
-o	Developed and prepared a cash flow budget for the 2020 fiscal year using Excel
-o	Created an automated data variance analysis script using Python to compare ADP and Open Path data payroll times, streamlining data analysis and reducing manual effort.
-o	Completed ad hoc data analysis projects to drive decision-making and risk management.
-
+DAVOR KONDIC
+Aurora, IL 60506 | 630-589-9913 | davorkondic@rocketmail.com
+o Extracted, transformed, and analyzed accounting, inventory, sales, and customer data from multiple sources (Quick
+Books Online, Fishbowl, V-Tiger)
+o Developed and prepared a cash flow budget for the 2020 fiscal year using Excel
+o Created an automated data variance analysis script using Python to compare ADP and Open Path data payroll times,
+streamlining data analysis and reducing manual effort.
+o Completed ad hoc data analysis projects to drive decision-making and risk management.
 Data Analyst / Compliance Auditor, Alliance for Audited Media, Arlington Heights, IL (2014 – 2019)
-o	Extracted, transformed, and loaded (ETL) print and digital media data for analysis and audit procedures, ensuring data quality and compliance.
-o	Cleaned raw media data using various analytical tools (Excel, Python, R, SPSS), demonstrating expertise in data wrangling and manipulation.
-o	Conducted structured audits to confirm compliance and data quality, mentoring and training new auditors and analysts to enhance team capabilities.
-o	Assisted in the development of a machine learning model to predict digital ad fraud
-"""
+o Extracted, transformed, and loaded (ETL) print and digital media data for analysis and audit procedures, ensuring
+data quality and compliance.
+o Cleaned raw media data using various analytical tools (Excel, Python, R, SPSS), demonstrating expertise in data
+wrangling, manipulation, and automation.
+o Conducted structured audits to confirm compliance and data quality, while mentoring and training new auditors.
+o Assisted in the development of a machine learning model to predict digital ad fraud"""
+
+
+
     messages=[{"role": "system", "content": content+resume}]
     for val in history:
         if val[0]:
@@ -64,7 +96,7 @@ o	Assisted in the development of a machine learning model to predict digital ad 
     messages.append({"role": "user", "content": message})
     
     output = client.chat.completions.create(
-    model="meta-llama/Meta-Llama-3.1-70B-Instruct",
+    model="meta-llama/Llama-3.3-70B-Instruct",
     messages=messages,
     stream=True,
     max_tokens=1024,)
@@ -73,10 +105,10 @@ o	Assisted in the development of a machine learning model to predict digital ad 
     response = ""
     for chunk in output:
         response += chunk.choices[0].delta.content or ""
-    print(response)
-    return response
+        yield response
+    print("Assistant Message: ",response,"\n")
 
-demo = gr.ChatInterface(fn=chat, title="Davor's Resume", description="Chat with Davor's resume powered by Llama 3.1 70B.",
+demo = gr.ChatInterface(fn=chat, title="Resume Chatbot", description="Chat with Davor's resume powered by Llama-3.3-70B-Instruct.",
                         stop_btn="Stop Generation", multimodal=False)
 
 if __name__ == "__main__":
